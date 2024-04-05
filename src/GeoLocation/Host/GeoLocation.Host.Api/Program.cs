e2cae01ec109a62401f39f20e4ce3d5070f7d81a
PavelDikunets <dikunets.pavel@gmail.com>
2024-04-05 18:16:55 +0300

    Добавил сервисы, настройку Swagger и документации, middleware для обработки исключений

--- src/GeoLocation/Host/GeoLocation.Host.Api/Program.cs
+++ src/GeoLocation/Host/GeoLocation.Host.Api/Program.cs
@@ -1,3 +1,9 @@
+using GeoLocation.Application.AppData.Contexts.GeoLocationData.Services;
+using GeoLocation.Clients.ExternalApi.Nominatim.Services;
+using GeoLocation.Contracts.AddressDtos;
+using GeoLocation.Host.Api.Middlewares;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IOpenStreetMapService, OpenStreetMapService>();
+builder.Services.AddScoped<IGeoLocationDataService, GeoLocationDataService>();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "GeoLocationDataCustomerService Api", Version = "V1" });
+    options.IncludeXmlComments(Path.Combine(Path.Combine(AppContext.BaseDirectory,
+        $"{typeof(AddressDto).Assembly.GetName().Name}.xml")));
+    options.IncludeXmlComments(Path.Combine(Path.Combine(AppContext.BaseDirectory, "Documentation.xml")));
+});
 
 var app = builder.Build();
 
@@ -21,5 +38,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.Run();
